b9cad2e427ac840c6b0bb91d9f53fcf03dd17278
Zaid Khan <brain@zaidkhan.me>
2019-04-21 15:20:49 -0700

    Added line to move to knife.

--- autofletch.py
+++ autofletch.py
@@ -4,6 +4,12 @@ import subprocess
 from dataclasses import dataclass
 import pyscreenshot as pyss 
 import py_prtscn
+import os
+from PIL import Image
+import pyautogui as agui
+
+source_dir = os.path.dirname(os.path.realpath(__file__))
+data_dir = os.path.join(source_dir, 'data')
 
 @dataclass(frozen=True)
 class Window:
@@ -27,4 +33,9 @@ window_geometry = yaml.load(window_geometry_raw, Loader=Loader)
 
 window = Window(**window_geometry)
 im = window.take_screenshot()
-im.show()
+
+knife_image = Image.open(os.path.join(data_dir, 'Knife.png'))
+
+left, top, width, height = agui.locate(knife_image, im)
+agui.moveTo(window.x + 2 + left, window.y + top + 3, duration=1, tween=agui.easeInElastic)
+agui.click()
