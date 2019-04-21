297a3af378773811d78d826636debe36d61c2a4a
Zaid Khan <brain@zaidkhan.me>
2019-04-21 14:48:17 -0700

    Added autofletch script.

---
+++ autofletch.py
@@ -0,0 +1,28 @@
+import yaml
+from yaml import Loader 
+import subprocess
+from dataclasses import dataclass
+import pyscreenshot as pyss 
+
+@dataclass(frozen=True)
+class Window:
+    x: int
+    y: int
+    windowId: int
+    width: int
+    height: int
+
+    def get_bounding_box(self):
+        return self.x, self.y, self.x + self.width, self.y + self.height
+
+    def take_screenshot(self):
+        return pyss.grab(bbox=self.get_bounding_box())
+
+
+
+window_geometry_raw = subprocess.check_output('./get_window_geometry.sh', shell=True).decode('utf-8').strip()
+window_geometry = yaml.load(window_geometry_raw, Loader=Loader)
+
+window = Window(**window_geometry)
+im = window.take_screenshot()
+im.show()
