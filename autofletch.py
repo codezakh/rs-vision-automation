27fd3d51115b81399c6499dcd8630793afc3fbee
Zaid Khan <brain@zaidkhan.me>
2019-04-21 15:49:43 -0700

    Made the bounding box function more oo and added data dir.

--- autofletch.py
+++ autofletch.py
@@ -7,9 +7,13 @@ import py_prtscn
 import os
 from PIL import Image
 import pyautogui as agui
+import random
+from collections import namedtuple
 
 source_dir = os.path.dirname(os.path.realpath(__file__))
 data_dir = os.path.join(source_dir, 'data')
+
+Point = namedtuple('Point', ['x', 'y'])
 
 @dataclass(frozen=True)
 class Window:
@@ -23,8 +27,39 @@ class Window:
         return self.x, self.y, self.x + self.width, self.y + self.height
 
     def take_screenshot(self):
-        #return pyss.grab(bbox=self.get_bounding_box())
         return py_prtscn.grab_screen(*self.get_bounding_box())
+
+    def locate(self, img_to_locate):
+        self_screenshot = self.take_screenshot()
+        left, top, width, height = agui.locate(img_to_locate, self_screenshot, confidence=0.7)
+        located_bbox = BoundingBox(
+            left=left + self.x,
+            top=top + self.y,
+            width=width,
+            height=height
+        )
+
+        return located_bbox
+
+@dataclass
+class BoundingBox:
+    left: int
+    top: int
+    width: int
+    height: int
+
+    @property
+    def centroid(self):
+        return Point(x=self.left + self.width / 2, y=self.top + self.height / 2)
+
+    def move_to_center(self):
+        jittered_center = jitter(self.centroid)
+        return agui.moveTo(jittered_center.x, jittered_center.y, duration=0.5, tween=agui.easeInElastic)
+
+def jitter(point, x_radius=10, y_radius=10):
+        x_jittered = point.x + random.uniform(-x_radius, x_radius)
+        y_jittered = point.y + random.uniform(-y_radius, y_radius)
+        return Point(x=x_jittered, y=y_jittered)
 
 
 
@@ -36,6 +71,10 @@ im = window.take_screenshot()
 
 knife_image = Image.open(os.path.join(data_dir, 'Knife.png'))
 
-left, top, width, height = agui.locate(knife_image, im)
-agui.moveTo(window.x + 2 + left, window.y + top + 3, duration=1, tween=agui.easeInElastic)
-agui.click()
+# left, top, width, height = agui.locate(knife_image, im)
+# agui.moveTo(window.x + 2 + left, window.y + top + 3, duration=1, tween=agui.easeInElastic)
+# agui.click()
+
+knife_bbox = window.locate(knife_image)
+knife_bbox.move_to_center()
+#agui.moveTo(x=knife_bbox.centroid.x, y=knife_bbox.centroid.y, duration=2, tween=agui.easeInElastic)
